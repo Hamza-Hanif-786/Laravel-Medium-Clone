1faf8e1eaf64d1ee211ae9a2fd22a7928fa10e81
Hamza Hanif <hamzahanif1024@gmail.com>
2025-05-24 12:28:24 +0500

    Add create post view template

---
+++ resources/views/post/create.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <div class="py-4">
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xs sm:rounded-lg">
+                <div class="p-4 text-gray-900 dark:text-gray-100">
+                    Create a New Post
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
