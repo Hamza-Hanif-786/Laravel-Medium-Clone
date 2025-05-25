e3066a4a6f72d4c50c36dcb6132324e75e6a6515
Hamza Hanif <hamzahanif1024@gmail.com>
2025-05-25 11:58:25 +0500

    Generate unique username during user registration

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
 
@@ -29,13 +30,16 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        $request->validate([
+        $data =$request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        $data['username'] = Str::slug($data['name'], '_') . '_' . Str::random(4);
+
         $user = User::create([
+            'username' => $data['username'],
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
