227ad8aab691235f0733df4cb3884f4c7961b1f9
Hamza Hanif <hamzahanif1024@gmail.com>
2025-05-25 19:51:08 +0500

    Enhance profile update form to include image upload and bio field, and improve localization handling

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,11 +1,11 @@
 <section>
     <header>
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-            {{ __('Profile Information') }}
+            {{ ('Profile Information') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            {{ __("Update your account's profile information and email address.") }}
+            {{ ("Update your account's profile information and email address.") }}
         </p>
     </header>
 
@@ -13,42 +13,72 @@
         @csrf
     </form>
 
-    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6" enctype="multipart/form-data">
+        {{-- CSRF Protection --}}
         @csrf
         @method('patch')
 
+        {{-- Profile Photo --}}
+        @if ($user->image)
+            <div class="mb-4">
+                <img src="{{ Storage::url($user->image) }}" alt="User Avatar" class="size-28 rounded-full object-cover">
+            </div>
+        @endif
+
+        {{-- Image  --}}
+        <div>
+            <x-input-label for="image" :value="('Avatar')" />
+            <x-text-input id="image" :value="old('image')" class="block border mt-1 w-full file:mr-4 file:py-2 file:px-4 file:rounded-lg file:border file:border-gray-300 file:bg-gray-800 file:focus:bg-gray-700 file:active:bg-gray-900 file:text-white" type="file" name="image" />
+            <x-input-error :messages="$errors->get('image')" class="mt-2" />
+        </div>
+
+        {{-- UserName --}}
+        <div>
+            <x-input-label for="username" :value="__('UserName')" />
+            <x-text-input id="username" name="username" type="text" class="mt-1 block w-full" :value="old('username', $user->username)" required autofocus autocomplete="username" />
+            <x-input-error class="mt-2" :messages="$errors->get('username')" />
+        </div>
+
+        {{-- Name --}}
         <div>
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
         </div>
 
+        {{-- Email --}}
         <div>
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
-
             @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
                 <div>
                     <p class="text-sm mt-2 text-gray-800 dark:text-gray-200">
-                        {{ __('Your email address is unverified.') }}
-
+                        {{ ('Your email address is unverified.') }}
                         <button form="send-verification" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
-                            {{ __('Click here to re-send the verification email.') }}
+                            {{ ('Click here to re-send the verification email.') }}
                         </button>
                     </p>
-
                     @if (session('status') === 'verification-link-sent')
                         <p class="mt-2 font-medium text-sm text-green-600 dark:text-green-400">
-                            {{ __('A new verification link has been sent to your email address.') }}
+                            {{ ('A new verification link has been sent to your email address.') }}
                         </p>
                     @endif
                 </div>
             @endif
         </div>
 
+        {{-- Bio --}}
+        <div>
+            <x-input-label for="bio" :value="('Bio')" />
+            <x-text-area id="bio" class="block mt-1 w-full" name="bio" rows="10" :value="old('bio')" placeholder="Enter Your Bio">
+                {{ old('bio', $user->bio) }}
+            </x-text-area>
+            <x-input-error :messages="$errors->get('bio')" class="mt-2" />
+        </div>
+
         <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
+            <x-primary-button>{{ ('Save') }}</x-primary-button>
 
             @if (session('status') === 'profile-updated')
                 <p
@@ -57,7 +87,7 @@
                     x-transition
                     x-init="setTimeout(() => show = false, 2000)"
                     class="text-sm text-gray-600 dark:text-gray-400"
-                >{{ __('Saved.') }}</p>
+                >{{ ('Saved.') }}</p>
             @endif
         </div>
     </form>
