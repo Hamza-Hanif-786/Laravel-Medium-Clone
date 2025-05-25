af0003bda4a59dcde3e40d463135aa8f83706bbb
Hamza Hanif <hamzahanif1024@gmail.com>
2025-05-25 11:36:25 +0500

    Comment out post factory creation in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,7 +33,7 @@ class DatabaseSeeder extends Seeder
             Category::create(['name' => $category]);
         }
 
-        Post::factory(100)->create();
+        // Post::factory(100)->create();
 
         // $this->call([                     // Example of calling another seeder
         //     PostSeeder::class
