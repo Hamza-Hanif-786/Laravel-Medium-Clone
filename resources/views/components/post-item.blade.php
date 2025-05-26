60675d5f0f808f09dfd5f62f10af74ad31b114f8
Hamza Hanif <hamzahanif1024@gmail.com>
2025-05-26 11:48:03 +0500

    Add aspect ratio class to post image for improved display

--- resources/views/components/post-item.blade.php
+++ resources/views/components/post-item.blade.php
@@ -1,5 +1,5 @@
 <div class="bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-    <img class="rounded-t-lg w-full h-48 object-cover" src={{ Storage::url($post->image) ?? "https://flowbite.com/docs/images/blog/image-1.jpg" }} alt="" />
+    <img class="rounded-t-lg w-full h-48 object-cover aspect-video" src={{ Storage::url($post->image) ?? "https://flowbite.com/docs/images/blog/image-1.jpg" }} alt="" />
     <div class="p-5">
         <a href="#">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $post->title }}</h5>
