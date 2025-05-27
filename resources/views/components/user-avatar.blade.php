789c468f60737d6035e77a6582b0fa991698ad06
Hamza Hanif <hamzahanif1024@gmail.com>
2025-05-27 08:50:52 +0500

    Enhance user avatar component to accept dynamic size prop for improved flexibility

--- resources/views/components/user-avatar.blade.php
+++ resources/views/components/user-avatar.blade.php
@@ -1,7 +1,11 @@
-@props(['user'])
+@props(['user', 'size' => 'size-14'])
+
+{{-- User Avatar Component --}}
+{{-- Displays the user's avatar with a default image if none is provided --}}
+{{-- Usage: <x-user-avatar :user="$user" :size="size-28" /> --}}
 
 @if ($user->image)
-    <img src="{{ $user->imageUrl() }}" alt="{{ $user->name }}" class="size-14 rounded-full mr-2">
+    <img src="{{ $user->imageUrl() }}" alt="{{ $user->name }}" class="{{ $size }} rounded-full">
 @else
-    <img src="{{ asset('default_avatar.png') }}" alt="{{ $user->name }}" class="size-14 rounded-full mr-2">
+    <img src="{{ asset('default_avatar.png') }}" alt="{{ $user->name }}" class="{{ $size }} rounded-full">
 @endif
