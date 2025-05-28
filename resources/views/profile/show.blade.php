b8403a3d31c7955038b3cc1cb881bb89208ba3f8
Hamza Hanif <hamzahanif1024@gmail.com>
2025-05-28 20:51:56 +0500

    Enhance user profile page with dynamic follow/unfollow button and follower count

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -15,18 +15,33 @@
                             @endforelse
                         </div>
                     </div>
-                    <div class="w-[25%] border-l pl-8">
+                    <div class="w-[25%] border-l pl-8" 
+                    x-data="{ 
+                        following: {{ $user->isFollowedBy(auth()->user()) ? 'true' : 'false' }},
+                        followersCount: {{ $user->followers->count() }},
+                        follow() {
+                            this.following = !this.following;
+                            axios.post('/follow/{{ $user->id }}').then(response => {
+                                console.log(response.data)
+                                this.followersCount = response.data.followers;
+                            }).catch(error => {
+                                console.error(error);
+                            });
+                        } 
+                    }">
                         <x-user-avatar :user="$user" size="size-24" />
                         <h3 class="font-bold mt-4 text-lg">{{ $user->name }}</h3>
-                        <p class="text-gray-500 mt-2 text-lg">{{ $user->followers->count() }} Followers</p>
+                        <p class="text-gray-500 mt-2 text-lg"><span x-text="followersCount"></span> Followers</p>
                         <p class="text-gray-800 mt-2 text-lg">
                             {{ $user->bio ?? 'No bio available' }}
                         </p>
-                        <div class="mt-4">
-                            <button class="px-4 py-2 bg-emerald-600 text-white rounded-full hover:bg-emerald-700 transition-colors">
-                                Follow
-                            </button>
-                        </div>
+                        @if (auth()->user() && auth()->user()->id !== $user->id)
+                            <div class="mt-4">
+                                <button x-text="following ? 'Unfollow' : 'Follow'" x-on:click="follow()" :class="following ? 'bg-emerald-800 hover:bg-emerald-900' : 'bg-emerald-600 hover:bg-emerald-700'"
+                                    class="px-4 py-2 cursor-pointer bg-emerald-600 text-white rounded-full hover:bg-emerald-700 transition-colors">
+                                </button> 
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
